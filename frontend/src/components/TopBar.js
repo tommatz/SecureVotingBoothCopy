461b61a363c8a441e5a57627d47b25e5f6891aaf
Gerry0191 <pas4hire@gmail.com>
2022-10-21 14:33:08 -0400

    Fixed dark gradient

--- frontend/src/components/TopBar.js
+++ frontend/src/components/TopBar.js
@@ -6,7 +6,7 @@ const TopBar = ({darkMode, setDarkMode}) => {
   /*This return statement is what gets passed up to the App.js then to the index.js
   it is then displayed on the user's screen as the html within the return statement*/
   return (
-    <section id="topbar" className="bg-gradient-to-b from-green-500 to-green-600 dark:from-slate-700 dark:to-slate-500 h-[5%] w-full">
+    <section id="topbar" className="bg-gradient-to-b from-green-500 to-green-600 dark:from-slate-500 dark:to-slate-700 h-[5%] w-full">
       <div className="flex flex-row h-full w-full items-center px-4 space-x-4 dark:text-white font-bold text-sm sm:text-base md:text-xl">
         <div className="flex flex-row h-full w-full items-center justify-between">
           <p>{name}</p>
