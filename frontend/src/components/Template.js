851d9f99bcb194b3a922eb97ecbfc3fee1a910a3
Gerry0191 <pas4hire@gmail.com>
2022-10-21 14:17:47 -0400

    Updated formatting of template page and top bar. Added toggled dark mode.

--- frontend/src/components/Template.js
+++ frontend/src/components/Template.js
@@ -5,11 +5,11 @@ const Template = () => {
     /*This return statement is what gets passed up to the App.js then to the index.js
     it is then displayed on the user's screen as the html within the return statement*/
     return (
-        <section id="template">
-            <div className="flex flex-col p-4">
-                <div className="flex flex-col space-y-4">
+        <section id="template" className="h-[95%] w-full"> {/* The 95% vertical height of the template section is to fill the rest of the screen (top bar takes 5%) */}
+            <div className="flex flex-col space-y-4 h-full w-full">
+                <div className="m-auto text-center dark:text-white"> {/* This div is needed to center the items inside it using m-auto (margins auto) */}
                     <h1 className="text-4xl font-bold">Header</h1>
-                    <p className="text-xl text-gray-700">subtext</p>
+                    <p className="text-xl text-neutral-700 dark:text-neutral-400">subtext</p>
                 </div>
             </div>
         </section>
--- frontend/src/components/TopBar.js
+++ frontend/src/components/TopBar.js
@@ -1,15 +1,20 @@
-const TopBar = () => {
+const TopBar = ({darkMode, setDarkMode}) => {
 
-  //Any page JSX would go up here
+  const name = "Name/ID here"
+  const location = "Location here"
 
   /*This return statement is what gets passed up to the App.js then to the index.js
   it is then displayed on the user's screen as the html within the return statement*/
   return (
-    <section id="topbar" className="bg-green-800">
-      <div className="flex flex-row px-4 justify-between text-xl">
-        <p>Name/ID</p>
-        <p>City/Country</p>
-        <p>State</p>
+    <section id="topbar" className="bg-gradient-to-b from-green-500 to-green-600 dark:from-slate-700 dark:to-slate-500 h-[5%] w-full">
+      <div className="flex flex-row h-full w-full items-center px-4 space-x-4 dark:text-white font-bold text-sm sm:text-base md:text-xl">
+        <div className="flex flex-row h-full w-full items-center justify-between">
+          <p>{name}</p>
+          <p>ElectionGuard</p>
+          <p>{location}</p>
+        </div>
+        <span className="h-full w-1 bg-black dark:bg-white" />
+        <button onClick={() => setDarkMode(!darkMode)} > B </button>
       </div>
     </section>
   )
