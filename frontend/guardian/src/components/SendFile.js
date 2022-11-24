377515ada6fc090fa0057188cc17b6560dee0486
Gerry0191 <pas4hire@gmail.com>
2022-11-23 21:07:21 -0500

    Finished SendFile logic

--- frontend/guardian/src/components/SendFile.js
+++ frontend/guardian/src/components/SendFile.js
@@ -1,29 +1,56 @@
-const SendFile = (url, file) => {
+import { useState } from "react";
 
-    const requestOptions = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ file })
-    };
+const SendFile = ({url}) => {
 
-    fetch(url + "/voter/setup_contest", requestOptions)
-        .then(async response => {
-            const isJson = response.headers.get('content-type')?.includes('application/json');
-            const data = isJson && await response.json();
+    const [file, setFile] = useState()
 
-            // check for error response
-            if (!response.ok) {
-                // get error message from body or default to response status
-                const error = (data && typeof (data) == "object" && data["detail"][0]["msg"]) || data || response.status;
-                return Promise.reject(error);
-            }
+    const handleChange = (e) => {
+        if (e.target.files) 
+            setFile(e.target.files[0])
+    }
 
-        })
-        .catch(error => {
-            return (error);
-        });
+    const handleSubmit = () => {
+        if(!file || file.type !== "application/json") return
+console.log(file)
+const data = new FormData();
 
-    return (response);
+  data.append("manifest", file, "manifest.json");
+
+        const requestOptions = {
+            method: 'POST',
+            body: data,
+           /* headers: {
+                'content-type': file.type,
+                'content-length': `${file.size}`,
+              },*/
+        };
+        fetch(url + "/guardian/setup_election", requestOptions)
+            .then(async res => {
+                const isJson = res.headers.get('content-type')?.includes('application/json');
+                const data = isJson && await res.json();
+
+                // check for error response
+                if (!res.ok) {
+                    // get error message from body or default to response status
+                    const error = data || res.status;
+                    return Promise.reject(error);
+                }
+
+                console.log(res)
+                return (res);
+            })
+            .catch(error => {
+                console.log(error)
+                return (error);
+            });
+    }
+
+    return (
+        <div>
+            <input type="file" onChange={handleChange} />
+            <button onClick={handleSubmit} className="border-2 border-black">Upload File</button>
+        </div>
+    )    
 }
 
 export default SendFile
