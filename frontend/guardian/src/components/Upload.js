1e84c1f396189ce272d406187f28384cd9d9d0bb
ln077218 <ln077218@ohio.edu>
2022-11-28 00:12:58 -0500

    finished issue 35

--- frontend/guardian/src/components/Upload.js
+++ frontend/guardian/src/components/Upload.js
@@ -1,9 +1,19 @@
 import SendFile from "./SendFile"
+import TopBar from "./TopBar"
 
-const Upload = ({url}) => {
+const Upload = ({ url }) => {
     return (
         <section id="upload" className="h-full w-full">
-            <SendFile url={url}/>
+            <TopBar />
+
+            <section id="upload-content" className="h-[90%] w-full flex flex-col">
+                <div className="m-auto text-center space-y-12"> {/* This div is needed to center the items inside it using m-auto (margins auto) */}
+                    <h1 className="text-4xl font-bold">Manifest Upload Page</h1>
+                    <SendFile url={url} />
+                </div>
+            </section>
+
+            <section id="bottombar" className="h-[5%] w-full flex flex-row-reverse items-center bg-green-600 border-t-4 border-black"></section>
         </section>
     )
 }
