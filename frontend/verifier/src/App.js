ff6dae1bb3cc750c7d98382fe2491dfb5d017582
Gerry0191 <pas4hire@gmail.com>
2023-02-23 13:01:40 -0500

    Added Verifier React App

---
+++ frontend/verifier/src/App.js
@@ -0,0 +1,11 @@
+import Template from "./components/Template";
+
+function App() {
+  return (
+    <header id="Verifier-App" className="h-screen w-screen">
+      <Template/>
+    </header>
+  );
+}
+
+export default App;
---
+++ frontend/verifier/src/components/Template.js
@@ -0,0 +1,19 @@
+const Template = () => {
+
+    //Any page JSX would go up here
+
+
+    /*This return statement is what gets passed up to the App.js then to the index.js
+    it is then displayed on the user's screen as the html within the return statement*/
+
+    return (
+        <section id="template" className="h-full w-full flex flex-col space-y-4">
+            <div className="m-auto text-center text-black dark:text-white transition-colors duration-500"> {/* This div is needed to center the items inside it using m-auto (margins auto) */}
+                <h1 className="text-4xl font-bold">Template Page</h1>
+                <p className="text-xl text-neutral-700 dark:text-neutral-400 transition-colors duration-500">for the verifier</p>
+            </div>
+        </section>
+    )
+}
+
+export default Template
